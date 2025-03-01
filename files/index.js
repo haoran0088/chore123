40a89e4dc62a19b2b2883d1bad191f8129b63c50
haoran0088 <ayucv8470@gmail.com>
2025-03-01 04:06:42 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
-const UUID = process.env.UUID || '4c5da4a5-d5f2-4fe9-b825-4be3b0a4b771'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '3adb813a-355a-494c-b268-db1fc041716c'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '15-002.didids.ip-ddns.com';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZjVlNDE2ODhhM2I4NjAwNjA2NWIzNjNhYTA5MmQ4MzYiLCJ0IjoiODM4NmY5ODMtZDhjNi00NTcyLWI2ZTMtZDgyY2RkZGUxM2UzIiwicyI6Ik5qSTVZekZtWkRBdE1qZzBZeTAwTVRCaUxUazNZell0T0ROaU1ERXdZakE0TUdNdyJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '0001.zooouik1.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNmIxYTBjOWEwYmI2NzhmYjMyNjQwNTkwMTI1MzFkODMiLCJ0IjoiNmQzMDFjODgtMzQ1OS00YmU4LTg0Y2EtZjZjZTUwY2NhM2Q0IiwicyI6Ik1EUm1ZakU0Wm1NdFpUUmtaQzAwTTJFekxXRTVPVFl0TlRSaE5qVTROR1UxT1RBMiJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
